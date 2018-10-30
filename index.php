e073f6ad665025b34f08752ad91b8082f7b4b8bb
Liyun Dai <dlyun2009@gmail.com>
2018-10-30 19:16:54 +0800

    add files

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-<?php
-$mysql = new mysqli("localhost", "root");
-echo "MySQL Server info: ".$mysql->host_info;
-echo "dailiyun123"
-?>
+<!doctype html>
+<html>
+  
+<head>
 
-
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>大作业题目</title>
+</head>
+<body>
+  <p><a href="./cases.php">题目</a> </p>
+  <p><a href="./cases.php">报名</a> </p>
+</body>
+</html>
 
